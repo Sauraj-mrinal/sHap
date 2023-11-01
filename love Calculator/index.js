36a63ed368b1fc3c071d59f9970e5392aa0b6850
Sauraj-mrinal <msauraji@gmail.com>
2023-11-01 19:38:40 +0530

    ADD

---
+++ love Calculator/index.js
@@ -0,0 +1,6 @@
+prompt("what is ypur name ");
+prompt("what is their name ?");
+var loveScore = Math.random();
+let val = loveScore *100;
+var loveScore = Math.floor(val)+1;
+alert("yout love score is"+loveScore+"%");
